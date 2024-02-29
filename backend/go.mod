f5a6aa7ccb3439f1582e726c7e5380d4b64e683f
xWalian <lukasek008@gmail.com>
2024-02-29 12:45:54 +0100

    adding postgresql

--- backend/go.mod
+++ backend/go.mod
@@ -2,6 +2,8 @@ module main
 
 go 1.21.1
 
+require github.com/xWalian/GoProject v0.0.0-20240229093750-13449b60a6b3
+
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.2.1 // indirect
@@ -14,10 +16,19 @@ require (
 	github.com/go-openapi/spec v0.20.14 // indirect
 	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/gofiber/fiber/v2 v2.52.1 // indirect
+	github.com/gofiber/storage/postgres/v3 v3.0.0-20240229075458-90157b80b79c // indirect
 	github.com/gofiber/swagger v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.3 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -32,13 +43,15 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.52.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	github.com/xWalian/GoProject v0.0.0-20240229093750-13449b60a6b3
 	github.com/xrash/smetrics v0.0.0-20231213231151-1d8dd44e695e // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.18.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.7 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,40 +1,101 @@
 package main
 
 import (
-	_ "github.com/xWalian/GoProject/backend/docs"
+	"log"
+	"main/src/application/storage"
+	"net/http"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
+	"github.com/joho/godotenv"
+	_ "github.com/xWalian/GoProject/backend/docs"
+	"github.com/xWalian/GoProject/backend/src/application/models"
+	"github.com/xWalian/GoProject/backend/src/application/storage"
+	"gorm.io/gorm"
 )
 
-// @title Fiber Example API
-// @version 1.0
-// @description This is a sample swagger for Fiber
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.email fiber@swagger.io
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
-// @BasePath /
+type Order struct {
+	Product_id int `json: "product_id"`
+	Quantity   int `json: "quantity"`
+	Owner      int `json: "owner"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) CreateOrder(context *fiber.Ctx) error {
+	order := Order{}
+	err := context.BodyParser(&order)
+
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
+	}
+
+	err = r.DB.Create(&order).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create order"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{"message": "book has been added"})
+	return nil
+}
+
+func (r *Repository) GetOrders(context *fiber.Ctx) error {
+	orderModels := &[]models.Orders{}
+	err := r.DB.Find(orderModels).Error
+
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "could not get orders"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "orders fetched successfully",
+		"data":    orderModels,
+	})
+	return nil
+}
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create-order", r.CreateOrder)
+	api.Delete("delete-order", r.DeleteOrder)
+	api.Get("/get-orders/:id", r.GetOrderById)
+	api.Get("/orders", r.GetOrders)
+}
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := Repository{
+		DB: db,
+	}
+	config := &storage.Config{
+		Host:     os.Gatenv("DB_HOST"),
+		Port:     os.Gatenv("DB_PORT"),
+		Password: os.Gatenv("DB_PASS"),
+		User:     os.Gatenv("DB_USER"),
+		SSLMode:  os.Gatenv("DB_SSLMODE"),
+		DBName:   os.Gatenv("DB_NAME"),
+	}
+	db, err := storage.NewConnection(config)
+
+	if err != nil {
+		log.Fatal("Could not load the database")
+	}
+
 	app := fiber.New()
 
-	app.Get("/swagger/*", swagger.HandlerDefault) // default
+	app.Get("/swagger/*", swagger.HandlerDefault)
 
-	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
-		URL:         "http://example.com/doc.json",
-		DeepLinking: false,
-		// Expand ("list") or Collapse ("none") tag groups by default
-		DocExpansion: "none",
-		// Prefill OAuth ClientId on Authorize popup
-		OAuth: &swagger.OAuthConfig{
-			AppName:  "OAuth Provider",
-			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
-		},
-		// Ability to change OAuth2 redirect uri location
-		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
-	}))
-
+	r.SetupRoutes(app)
 	app.Listen(":8080")
 }
